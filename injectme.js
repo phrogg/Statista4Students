63ee7d7a62821955590333ab631723a571b605a9
Phil Roggenbuck <ceo@roggstar.eu>
2019-10-15 14:19:11 +0200

    Updated for new statista backend
    It was not working anymore. It is again.

--- injectme.js
+++ injectme.js
@@ -1,17 +1,16 @@
 var divs = document.getElementsByTagName("li");
 
 for(var i = 0; i < divs.length; i++){
-	if(divs[i].getAttribute("class") == "list__item list__item--searchContentTypeStatistic"){
+	if(divs[i].getAttribute("class") != null && divs[i].getAttribute("class").indexOf("list__item list__item") != -1){
 		var cdivs = divs[i].getElementsByTagName("div");
 		var cis = divs[i].getElementsByTagName("i");
-		if(cdivs.length > 1 && cdivs[2].getAttribute("class") == "premiumBadge"){
+		if(cdivs.length > 1 && cdivs[2].getAttribute("class").indexOf("premium") != -1){
 			divs[i].remove();
 			i = 0;
 		}
-		if(cis.length > 0 && cis[0].getAttribute("class") == "list__itemIcon list__itemIcon--26 iconSprite iconSprite--statisticPremium"){
+		if(cis.length > 0 && cis[0].getAttribute("class").indexOf("Premium") != -1){
 			divs[i].remove();
 			i = 0;
 		}
    }
 }
-//next level would be to also change the results count: <span class="text-normal">(621)</span>
